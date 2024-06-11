17426542e285e83651f6c5b6396634e7e55342fa
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-11 16:21:51 +0200

    move topic component

---
+++ front/src/app/pages/topic/topic.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { TopicComponent } from './topic.component';
+
+export const TOPIC_ROUTES: Routes = [
+  { path: '', component: TopicComponent },
+  // { path: ':index', component: UserDetailComponent },
+ 
+];
