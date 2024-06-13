cee16ad62dc91d8795438cf8b649b3479470b632
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-13 11:40:52 +0200

    make call in submit register and save token

--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -5,6 +5,10 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../services/auth.service';
+import { HttpEventType } from '@angular/common/http';
+import { TokenStorageService } from '../services/token-storage.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -19,7 +23,7 @@ export class RegisterComponent {
   registerForm: FormGroup;
   passwordPattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$';
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -28,9 +32,19 @@ export class RegisterComponent {
   }
 
   onSubmit(): void {
-    console.log(this.registerForm.controls['email'].value)
     if (this.registerForm.valid) {
-      console.log('Form Value', this.registerForm.value);
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (data) => {
+          console.log("ok");
+          if (data.type == HttpEventType.Response) {
+            this.tokenStorage.saveToken(data.body.token);
+            this.router.navigateByUrl('/topics');
+          }
+        },
+        error: (e) => {
+          console.log(e);
+        }
+      })
     }
   }
 }
