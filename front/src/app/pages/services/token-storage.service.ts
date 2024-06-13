b636e0924ee0db47ebfe92ccde7bc4b853e3d10c
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-13 11:40:23 +0200

    correct token storage service

--- front/src/app/pages/services/token-storage.service.ts
+++ front/src/app/pages/services/token-storage.service.ts
@@ -10,7 +10,7 @@ export class TokenStorageService {
   constructor() {}
 
   /**
-   * called at the deconnexion, clear the session storage of token and user information.
+   * called at the deconnexion, clear the session storage of token.
    */
   signOut(): void {
     window.sessionStorage.clear();
@@ -31,27 +31,5 @@ export class TokenStorageService {
    */
   public getToken(): string | null {
     return window.sessionStorage.getItem(TOKEN_KEY);
-  }
-
-  /**
-   * save the user (not his password) in the session storage
-   * @param user
-   */
-  public saveUser(user: any): void {
-    window.sessionStorage.removeItem(USER_KEY);
-    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
-  }
-
-  /**
-   * get the user from the session storage
-   * @returns
-   */
-  public getUser(): any {
-    const user = window.sessionStorage.getItem(USER_KEY);
-    if (user) {
-      return JSON.parse(user!);
-    } else {
-      return {};
-    }
   }
 }
