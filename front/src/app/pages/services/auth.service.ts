bed6108b8beb9133d4797786b10dae7bad7f0685
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-13 11:37:35 +0200

    add authService and provider in main.ts

---
+++ front/src/app/pages/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { LoginRequest } from '../interfaces/login-request';
+import { Observable } from 'rxjs';
+
+
+const BACKEND_URL= environment.backendUrl + "/auth"
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'Authorization',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  
+  /**
+   * login method
+   * @param username
+   * @param password
+   * @returns
+   */
+  login(loginRequest: LoginRequest): Observable<any> {
+    const req = new HttpRequest(
+      'POST',
+      BACKEND_URL + '/login',
+      loginRequest,
+      httpOptions
+    );
+    return this.http.request(req);
+  }
+
+  /**
+   * login method
+   * @param username
+   * @param password
+   * @returns
+   */
+  register(loginRequest: LoginRequest): Observable<any> {
+    const req = new HttpRequest(
+      'POST',
+      BACKEND_URL + '/register',
+      loginRequest,
+      httpOptions
+    );
+    return this.http.request(req);
+  }
+}
--- front/src/main.ts
+++ front/src/main.ts
@@ -3,13 +3,15 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from 'src/app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   
   providers:
      [
       provideRouter(routes),
-      provideAnimations()
+      provideAnimations(),
+      provideHttpClient()
   ]
 }).catch(err => console.error(err));
 ;
