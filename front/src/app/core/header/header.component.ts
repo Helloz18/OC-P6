e45c88a31d7b6a4bc8ce4db0e468fbd33dad1a4c
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-13 09:59:44 +0200

    management style header for mobile and computer

--- front/src/app/core/header/header.component.ts
+++ front/src/app/core/header/header.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterModule],
+  imports: [CommonModule, MatToolbarModule, RouterModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+ loggedIn: boolean = false;
 }
