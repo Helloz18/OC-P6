40666bcdabb15ac251b43ee772f64aadda3d9c9c
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-14 14:54:04 +0200

    get value of signal in component and add button in header

--- front/src/app/core/header/header.component.ts
+++ front/src/app/core/header/header.component.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { TokenStorageService } from 'src/app/pages/services/token-storage.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, RouterModule, MatIconModule],
+  imports: [CommonModule, MatToolbarModule, RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
- loggedIn: boolean = false;
+ 
+  // this will get the value of the signal loggedIn() and be informed thanks to computed for any change on this value
+  loggedIn = computed(() => this.tokenService.loggedIn());
+  
+ constructor(private tokenService: TokenStorageService) {}
 }
