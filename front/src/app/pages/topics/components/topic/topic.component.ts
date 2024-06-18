c97c838210d6ba6acb387a7c0d0ddfd092126333
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-18 09:54:12 +0200

    add matbuttonmodule in TopicComponent

--- front/src/app/pages/topics/components/topic/topic.component.ts
+++ front/src/app/pages/topics/components/topic/topic.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Topic } from "../../interfaces/topic";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-topic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.scss']
 })
