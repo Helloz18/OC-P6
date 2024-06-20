ad6c5154ac24d1f2a588f145e355e9552678ad0e
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-20 12:37:35 +0200

    add button create post

--- front/src/app/pages/posts/components/post-list/post-list.component.ts
+++ front/src/app/pages/posts/components/post-list/post-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
 export class PostListComponent {
+
+    constructor(private router: Router){}
+
+  createPost() {
+    this.router.navigateByUrl('posts/create');
+  }
 
 }
