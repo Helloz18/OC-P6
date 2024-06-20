cd43b57b68c7c82b91db8c5f2ea8c85f0e9f62e5
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-20 09:20:18 +0200

    add api information on topic controller

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -2,6 +2,14 @@ package com.openclassrooms.mddapi.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +18,8 @@ import com.openclassrooms.mddapi.model.Topic;
 import com.openclassrooms.mddapi.service.ITopicService;
 
 @RestController
+@SecurityRequirement(name = "Bearer Authentication")
+@Tag(name = "Topic controller", description = "Controller used to manage topic: getAll")
 @RequestMapping("/api/topic")
 public class TopicController {
 	
@@ -19,10 +29,16 @@ public class TopicController {
 		this.topicService = topicService;		
 	}
 
-	@GetMapping
-	public List<Topic> getTopics() {
-		return topicService.getTopics();
+
+	@GetMapping("")
+	@Operation(summary = "Get the list of all topics.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "a List of Topics",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = Topic.class)))
+	})
+	public ResponseEntity<List<Topic>> getTopics() {
+		List<Topic> topics = topicService.getTopics();
+		return ResponseEntity.ok(topics);
 	}
-	
-	
 }
