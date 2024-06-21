8e76294d70175986397b9f7345ee54f5bdd4cc8e
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-21 10:53:33 +0200

    add interface for PostForListDTO

--- front/src/app/pages/posts/interfaces/post-dto.ts
+++ front/src/app/pages/posts/interfaces/post-dto.ts
@@ -3,3 +3,11 @@ export interface PostCreateDTO {
     content: string,
     topicId: number
 }
+
+export interface PostForListDTO {
+    id: number,
+    title: string,
+    content: string,
+    author: string,
+    createdAt: string
+}
