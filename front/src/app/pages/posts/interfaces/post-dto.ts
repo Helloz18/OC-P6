9fe48af023fff9228ac117cd92dca6dfd5c6d1e4
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-21 13:43:16 +0200

    add interface for postDTO and CommentDTO

--- front/src/app/pages/posts/interfaces/post-dto.ts
+++ front/src/app/pages/posts/interfaces/post-dto.ts
@@ -11,3 +11,17 @@ export interface PostForListDTO {
     author: string,
     createdAt: string
 }
+
+export interface PostDTO {
+    title: string,
+    content: string,
+    author: string,
+    topicName: string,
+    createdAt: string,
+    comments: CommentDTO[]
+}
+
+export interface CommentDTO {
+    author: string,
+    content: string
+}
