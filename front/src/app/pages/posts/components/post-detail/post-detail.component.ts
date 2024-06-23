ec74d20f5198bb3aaf03e125aca9ec1351b4f9b7
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-23 17:45:48 +0200

    add postDetail html

--- front/src/app/pages/posts/components/post-detail/post-detail.component.ts
+++ front/src/app/pages/posts/components/post-detail/post-detail.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import { PostDTO } from '../../interfaces/post-dto';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.scss'
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
 
+  id: number = 0;
+  postDTO!: PostDTO;
+
+  constructor(private route: ActivatedRoute, private router: Router, private postService: PostService) {
+    this.id = this.route.snapshot.params['index'];
+  }
+
+  ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost() {
+    this.postService.getPostById(this.id).subscribe({
+      next: (postDTO) => {
+        this.postDTO = postDTO;
+      },
+      error: (error) => {
+        alert(error.error.message);
+      }
+    })
+  }
+
+  previous() {
+    this.router.navigateByUrl('/posts');
+  }
 }
