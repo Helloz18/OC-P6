40db121ebef9a47c929f1007642ed9936a9a59a2
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-24 11:19:19 +0200

    add arrow to order posts

--- front/src/app/pages/posts/components/post-list/post-list.component.ts
+++ front/src/app/pages/posts/components/post-list/post-list.component.ts
@@ -5,17 +5,19 @@ import { PostService } from '../../services/post.service';
 import { PostForListDTO } from '../../interfaces/post-dto';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from "../post/post.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-post-list',
     standalone: true,
     templateUrl: './post-list.component.html',
     styleUrl: './post-list.component.scss',
-    imports: [MatButtonModule, CommonModule, PostComponent]
+    imports: [MatButtonModule, CommonModule, PostComponent, MatIconModule]
 })
 export class PostListComponent implements OnInit {
 
   listOfPosts: PostForListDTO[] = [];
+  down: boolean = true;
 
     constructor(private router: Router, private postService: PostService){}
   
@@ -31,8 +33,20 @@ export class PostListComponent implements OnInit {
     this.postService.getListOfPostsToReadByUser().subscribe({
       next: (listOfPosts) => {
         this.listOfPosts = listOfPosts;
+        this.sortByDate();
       }
     })
   }
 
+  sortByDate() {
+    if(this.down) {
+    this.listOfPosts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+  } else {
+    this.listOfPosts.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+  }
+  }
+  trier() {
+    this.down = !this.down;
+    this.sortByDate();
+  }
 }
